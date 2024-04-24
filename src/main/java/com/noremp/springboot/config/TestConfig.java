6bf8fde1dbe4a0244a506f2936d68ed6454a39ba
apsaires <andre.p.aires@gmail.com>
2024-04-24 21:35:42 +0100

    Payment, one-to-one association

--- src/main/java/com/noremp/springboot/config/TestConfig.java
+++ src/main/java/com/noremp/springboot/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.noremp.springboot.entities.Category;
 import com.noremp.springboot.entities.Order;
 import com.noremp.springboot.entities.OrderItem;
+import com.noremp.springboot.entities.Payment;
 import com.noremp.springboot.entities.Product;
 import com.noremp.springboot.entities.User;
 import com.noremp.springboot.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 		
 	}
 }
