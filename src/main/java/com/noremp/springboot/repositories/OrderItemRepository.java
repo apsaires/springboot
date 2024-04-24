c6f05f9d2810a2bd9fbcfb94b1ca90470f49872e
apsaires <andre.p.aires@gmail.com>
2024-04-24 21:40:35 +0100

    Subtotal & Total methods

--- src/main/java/com/noremp/springboot/repositories/OrderItemRepository.java
+++ src/main/java/com/noremp/springboot/repositories/OrderItemRepository.java
@@ -2,7 +2,6 @@ package com.noremp.springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.noremp.springboot.entities.Order;
 import com.noremp.springboot.entities.OrderItem;
 import com.noremp.springboot.entities.pk.OrderItemPK;
 
