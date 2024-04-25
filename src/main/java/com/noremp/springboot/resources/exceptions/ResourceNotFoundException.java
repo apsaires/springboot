e4d10b9e88def3f80fdf93758226b52f02be1d0c
apsaires <andre.p.aires@gmail.com>
2024-04-25 12:16:02 +0100

    Exception handling - delete

--- src/main/java/com/noremp/springboot/resources/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/noremp/springboot/resources/exceptions/ResourceNotFoundException.java
@@ -1,10 +1,5 @@
 package com.noremp.springboot.resources.exceptions;
 
-import java.io.Serializable;
-import java.time.Instant;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/noremp/springboot/services/UserService.java
+++ src/main/java/com/noremp/springboot/services/UserService.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.noremp.springboot.entities.User;
 import com.noremp.springboot.repositories.UserRepository;
+import com.noremp.springboot.resources.exceptions.DatabaseException;
+import com.noremp.springboot.resources.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -30,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
